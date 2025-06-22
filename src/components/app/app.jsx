c5db43605e6760e61a49cfa024eb5107f9f048ae
Dana Kun <dana.liepinsh@gmail.com>
2025-06-22 21:10:05 +0200

    feat: start routing and refactor app adding home page

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,46 +1,24 @@
 import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { DndProvider } from 'react-dnd';
-import { HTML5Backend } from 'react-dnd-html5-backend';
-import styles from './app.module.css';
-import { BurgerIngredients } from '@components/burger-ingredients/burger-ingredients.jsx';
-import { BurgerConstructor } from '@components/burger-contructor/burger-constructor.jsx';
+import { Routes, Route } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import { AppHeader } from '@components/app-header/app-header.jsx';
-import { Preloader } from '@components/preloader/preloader.jsx';
 import { fetchIngredients } from '../../services/ingredientsSlice';
+import { Home } from '../../pages/home/home';
+// import styles from './app.module.css';
 
 export const App = () => {
 	const dispatch = useDispatch();
-	const { isLoading, hasError } = useSelector((state) => state.ingredients);
 
 	useEffect(() => {
 		dispatch(fetchIngredients());
 	}, [dispatch]);
 
 	return (
-		<div className={styles.app}>
+		<>
 			<AppHeader />
-			<h1
-				className={`${styles.title} text text_type_main-large mt-10 mb-5 pl-5`}>
-				Соберите бургер
-			</h1>
-			<main className={`${styles.main} pl-5 pr-5`}>
-				{isLoading ? (
-					<Preloader />
-				) : hasError ? (
-					<p className='text text_type_main-medium text_color_error'>
-						Произошла ошибка при загрузке ингредиентов. Пожалуйста, попробуйте
-						позже.
-					</p>
-				) : (
-					<>
-						<DndProvider backend={HTML5Backend}>
-							<BurgerIngredients />
-							<BurgerConstructor />
-						</DndProvider>
-					</>
-				)}
-			</main>
-		</div>
+			<Routes>
+				<Route path='/' element={<Home />} />
+			</Routes>
+		</>
 	);
 };
